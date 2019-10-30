ffd0761eb6ae98765d910f4e86fddd53d19e4741
Xuejie Xiao <xxuejie@gmail.com>
2019-10-30 05:14:25 +0000

    Only link C library, not full libc

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rm -f libsmth.{o,a} test
 g++ -Wall -c -o libsmth.o smth.cpp && ar rcs libsmth.a libsmth.o
-go build --ldflags '-extldflags "-static"' -o test main.go && rm -f libsmth.{o,a}
+go build -o test main.go
